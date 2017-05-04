5fe82d1434768498143787e05666385618dac79d
ljingan <519201374@qq.com>
2017-05-04 19:00:56 +0800

    first commit

---
+++ Rs/src/com/ynwi/ssh/serviceImpl/UserManagerImpl.java
@@ -0,0 +1,12 @@
+package com.ynwi.ssh.serviceImpl;
+
+import com.ynwi.ssh.beans.UserForm;
+import com.ynwi.ssh.service.UserManager;
+
+public class UserManagerImpl implements UserManager {
+
+	public void regUser(UserForm user) {
+
+	}
+
+}
